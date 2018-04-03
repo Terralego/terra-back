8a4ae1fd7f8ec9e8b21c8bf2bc42cfd1fee6a50f
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-03 14:49:13 +0200

    Enable permission for superusers

--- src/trrequests/permissions.py
+++ src/trrequests/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsOwnerOrStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, object):
-        if request.user.is_staff:
+        if request.user.is_staff or request.user.is_superuser:
             return True
 
         return object.owner == request.user
