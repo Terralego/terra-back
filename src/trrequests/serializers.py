87298035b15c8cdf2a0bba76e6b613a0418a4da6
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-03 14:37:09 +0200

    Delete forgiven debug print

--- src/trrequests/serializers.py
+++ src/trrequests/serializers.py
@@ -10,7 +10,6 @@ class RequestSerializer(serializers.ModelSerializer):
         request_user = self.context['request'].user
         self.fields['organization'].queryset = (
                             Organization.objects.filter(owner=request_user))
-        print(dir(self.fields['organization']))
 
     class Meta:
         model = Request
