41f67a7de7766125aeee9aa272899d82f8cbbc4f
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-10 17:29:31 +0200

    Adds support of importing GeoJSON files
     Can import multiples GeoJSON file in a newly created layer

---
+++ src/terra/management/commands/import_geojson.py
@@ -0,0 +1,54 @@
+import argparse
+
+from django.db import transaction
+from django.core.management.base import BaseCommand
+
+from terra.models import Layer
+
+
+class Command(BaseCommand):
+    help = 'Import Features from GeoJSON files'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-l', '--layer',
+                            action="store",
+                            required=True,
+                            help=("Name of created layer "
+                                  "containing GeoJSON datas"))
+        parser.add_argument('-s', '--schema',
+                            type=argparse.FileType('r'),
+                            action="store",
+                            required=True,
+                            help='File that description GeoJSON properties')
+        parser.add_argument('-g', '--geojson',
+                            nargs='+',
+                            type=argparse.FileType('r'),
+                            action="store",
+                            required=True,
+                            help='GeoJSON files to import')
+        parser.add_argument('--dry-run',
+                            action="store_true",
+                            help='Execute une dry-run mode')
+
+    @transaction.atomic()
+    def handle(self, *args, **options):
+
+        schema = options.get('schema').read()
+        layer_name = options.get('layer')
+        geojson_files = options.get('geojson')
+        dryrun = options.get('dry_run')
+
+        sp = transaction.savepoint()
+        self.import_datas(layer_name, schema, geojson_files)
+
+        if dryrun:
+            transaction.savepoint_rollback(sp)
+        else:
+            transaction.savepoint_commit(sp)
+
+    def import_datas(self, layer_name, schema, geojson_files):
+        layer = Layer.objects.create(name=layer_name, schema=schema)
+        print(layer)
+
+        for file in geojson_files:
+            layer.import_geojson(file)
--- src/terra/models.py
+++ src/terra/models.py
@@ -1,10 +1,22 @@
+import json
+
 from django.contrib.postgres.fields import JSONField
 from django.contrib.gis.db import models
+from django.contrib.gis.geos.geometry import GEOSGeometry
 
 
 class Layer(models.Model):
     name = models.CharField(max_length=256)
     schema = JSONField(default=dict, blank=True)
+
+    def import_geojson(self, geojson_file):
+        geojson = json.loads(geojson_file.read())
+        for feature in geojson.get('features', []):
+            Feature.objects.create(
+                layer=self,
+                properties=feature.get('properties', {}),
+                geom=GEOSGeometry(json.dumps(feature.get('geometry')))
+            )
 
 
 class Feature(models.Model):
