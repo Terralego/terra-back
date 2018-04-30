8437634ccc65c21217a31d37e838659bdb798e35
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-30 11:37:03 +0200

    #7 - First Working CI with docker

--- terracommon/project/settings/test.py
+++ terracommon/project/settings/test.py
@@ -29,7 +29,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.environ.get('POSTGRES_HOST') or 'db',
         'PORT': '',
     }
 }
