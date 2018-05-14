0afa5286cf4551433c402e33cfd1d3a4c5356240
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-14 14:53:05 +0200

    #29 - fix comments URL

--- terracommon/trrequests/urls.py
+++ terracommon/trrequests/urls.py
@@ -5,7 +5,8 @@ from rest_framework import routers
 router = routers.SimpleRouter()
 
 router.register(r'userrequest', RequestViewSet, base_name='request')
+router.register(r'userrequest/(?P<request_pk>\d+)/comment', CommentViewSet, base_name='comment')
+
 router.register(r'organization', OrganizationViewSet, base_name='organization')
-router.register(r'request/(?P<request_pk>\d+)/comment', CommentViewSet, base_name='comment')
 
 urlpatterns = router.urls
