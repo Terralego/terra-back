7d0fd902114b95b888c82aa79c41f4b547582f8a
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-18 09:47:41 +0200

    #7 - fix linting

--- terracommon/trrequests/urls.py
+++ terracommon/trrequests/urls.py
@@ -5,7 +5,8 @@ from .views import CommentViewSet, OrganizationViewSet, RequestViewSet
 router = routers.SimpleRouter()
 
 router.register(r'userrequest', RequestViewSet, base_name='request')
-router.register(r'userrequest/(?P<request_pk>\d+)/comment', CommentViewSet, base_name='comment')
+router.register(r'userrequest/(?P<request_pk>\d+)/comment',
+                CommentViewSet, base_name='comment')
 
 router.register(r'organization', OrganizationViewSet, base_name='organization')
 
