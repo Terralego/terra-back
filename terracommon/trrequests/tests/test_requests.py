5436beed1a5edbdb764da841ccb1f00cc5d68bf8
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-18 09:40:46 +0200

    #32 - add test for request submission

---
+++ terracommon/trrequests/tests/test_requests.py
@@ -0,0 +1,103 @@
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework.test import APIClient, force_authenticate
+
+from terracommon.terra.models import TerraUser, Layer
+from terracommon.trrequests.models import Organization
+
+class RequestTestCase(TestCase):
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                [
+                    2.30712890625,
+                    48.83579746243093
+                ],
+                [
+                    1.42822265625,
+                    43.628123412124616
+                ]
+                ]
+            }
+            },
+            {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                [
+                    [
+                    -1.549072265625,
+                    43.49676775343911
+                    ],
+                    [
+                    -2.340087890625,
+                    43.25320494908846
+                    ],
+                    [
+                    3.2244873046875,
+                    42.07783959017503
+                    ],
+                    [
+                    3.021240234375,
+                    42.577354839557856
+                    ],
+                    [
+                    -1.549072265625,
+                    43.49676775343911
+                    ]
+                ]
+                ]
+            }
+            },
+            {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                1.42822265625,
+                43.57641143300888
+                ]
+            }
+            }
+        ]
+    }
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = TerraUser.objects.create_user(
+            email='foo@bar.com',
+            password='123456'
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.organization = Organization.objects.create()
+        self.organization.owner.set([self.user, ])
+
+    def test_request_creation(self):
+        response = self.client.post(
+            reverse('request-list'),
+            {
+                'state': 0,
+                'properties': {
+                    'myproperty': 'myvalue',
+                },
+                'geojson': self.geojson,
+                'organization': [self.organization.pk]
+
+            }, format='json')
+
+        self.assertEqual(201, response.status_code)
+
+        layer_pk = response.data.get('layer')
+        self.assertIsInstance(layer_pk, int)
+        self.assertEqual(3, Layer.objects.get(pk=layer_pk).features.all().count())
