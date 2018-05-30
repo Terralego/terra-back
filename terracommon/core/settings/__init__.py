cd5078c8ee54d33e653e813ec03716a369563bd2
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-30 13:11:15 +0200

    #49 - Allow JWT refresh

--- terracommon/core/settings/__init__.py
+++ terracommon/core/settings/__init__.py
@@ -105,7 +105,7 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': timedelta(seconds=300),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
-    'JWT_ALLOW_REFRESH': False,
+    'JWT_ALLOW_REFRESH': True,
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
     'JWT_AUTH_COOKIE': None,
