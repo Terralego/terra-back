c691f205234c8a0d45f94a00f7c55a973b658267
Simon Prévidente <simon.previdente@makina-corpus.com>
2018-06-15 13:42:24 +0200

    Organize import csv arguments

--- terracommon/data_importers/management/commands/import_csv_features.py
+++ terracommon/data_importers/management/commands/import_csv_features.py
@@ -13,83 +13,77 @@ class Command(BaseCommand):
     help = _('Import insee data from csv to db.')
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            '--layer',
-            required=True,
-            action='store',
-            dest='layer',
-            help=_('Specify layer name')
-        )
-        parser.add_argument(
-            '--key',
-            required=True,
-            action='append',
-            dest='pk_properties',
-            help=_("Define primary keys of this data"
-                   "Example with companies:"
-                   "--key=SIREN --key=NIC"
-                   "Example with INSEE:"
-                   "--key=code_insee")
-        )
-        parser.add_argument(
-            '--init',
-            action='store_true',
-            dest='init',
-            default=False,
-            help=_('Improve performance of initial import')
-        )
-        parser.add_argument(
-            '--bulk',
-            action='store_true',
-            dest='bulk',
-            default=False,
-            help=_('Delete all stored INSEE data. DO NOT USE IN PROD.')
-        )
-        parser.add_argument(
-            '--source',
-            dest='source',
-            type=argparse.FileType('r', encoding='iso-8859-15'),
-            default=sys.stdin,
-            required=True,
-            help=_('Specify source file path'),
-        )
-        parser.add_argument(
-            '--delimiter',
-            action='store',
-            dest='delimiter',
-            default=';',
-            required=False,
-            help=_('Specify CSV delimiter')
-        )
-        parser.add_argument(
-            '--creations-per-transaction',
-            dest='creations_per_transaction',
-            type=int,
-            default=1000,
-            help=_('Number of operations per transaction')
-        )
-        parser.add_argument(
-            '--longitude',
-            required=False,
-            action='store',
-            dest='longitude',
-            help=_('Name of longitude column')
-        )
-        parser.add_argument(
-            '--latitude',
-            required=False,
-            action='store',
-            dest='latitude',
-            help=_('Name of latitude column')
-        )
-        parser.add_argument('--fast',
+        parser.add_argument('-b', '--bulk',
+                            action='store_true',
+                            dest='bulk',
+                            default=False,
+                            help=_("Delete all stored INSEE data,"
+                                   " DO NOT USE IN PROD.")
+                            )
+        parser.add_argument('-c', '--creations-per-transaction',
+                            dest='chunk_size',
+                            type=int,
+                            default=1000,
+                            help=_('Number of rows per transaction')
+                            )
+        parser.add_argument('-d', '--delimiter',
+                            action='store',
+                            dest='delimiter',
+                            default=';',
+                            required=False,
+                            help=_('Specify CSV delimiter')
+                            )
+        parser.add_argument('-f', '--fast',
                             action='store_true',
                             default=False,
-                            help="If present and it's not an initial import"
-                                 " will speed up features creation. But no"
-                                 " rollback is possible. If something broke"
-                                 " up during import, the import will stop "
-                                 " with half data in database.")
+                            help=_("If present and it's not an initial import"
+                                   " will speed up features creation. But no"
+                                   " rollback is possible. If something broke"
+                                   " up during import, the import will stop "
+                                   " with half data in database.")
+                            )
+        parser.add_argument('-i', '--init',
+                            action='store_true',
+                            dest='init',
+                            default=False,
+                            help=_('Improve performance of initial import')
+                            )
+        parser.add_argument('-k', '--key',
+                            required=True,
+                            action='append',
+                            dest='pk_properties',
+                            help=_("Define primary keys of this data."
+                                   " Example with companies:"
+                                   " --key=SIREN --key=NIC"
+                                   " Example with INSEE:"
+                                   " --key=code_insee")
+                            )
+        parser.add_argument('-l', '--layer',
+                            required=True,
+                            action='store',
+                            dest='layer',
+                            help=_('Specify layer name')
+                            )
+        parser.add_argument('-s', '--source',
+                            dest='source',
+                            type=argparse.FileType('r',
+                                                   encoding='iso-8859-15'),
+                            default=sys.stdin,
+                            required=True,
+                            help=_('Specify source file path'),
+                            )
+        parser.add_argument('--longitude',
+                            required=False,
+                            action='store',
+                            dest='longitude',
+                            help=_('Name of longitude column')
+                            )
+        parser.add_argument('--latitude',
+                            required=False,
+                            action='store',
+                            dest='latitude',
+                            help=_('Name of latitude column')
+                            )
 
     def handle(self, *args, **options):
         layer_name = options.get('layer')
@@ -110,10 +104,10 @@ class Command(BaseCommand):
                                      if v is not None}
 
         layer.from_csv_dictreader(
-            reader,
-            options.get('pk_properties'),
-            options.get('init'),
-            options.get('creations_per_transaction'),
-            options.get('fast'),
-            geometry_columns_filtered
+            reader=reader,
+            pk_properties=options.get('pk_properties'),
+            init=options.get('init'),
+            chunk_size=options.get('chunk_size'),
+            fast=options.get('fast'),
+            geometry_columns=geometry_columns_filtered
         )
