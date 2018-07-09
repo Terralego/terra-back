ceefadda1541b14b53db1689de0582350157a339
Simon Prévidente <simon.previdente@makina-corpus.com>
2018-07-09 13:56:13 +0200

    Add assertLogs on import_csv_features command

--- terracommon/data_importers/tests/test_dataimporter.py
+++ terracommon/data_importers/tests/test_dataimporter.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from django.core.management import call_command
@@ -8,12 +9,15 @@ class DataImporterTestCase(TestCase):
     def test_command_launch(self):
         test_file = os.path.join(os.path.dirname(__file__), 'test.csv')
 
-        call_command('import_csv_features',
-                     ('--operation=terracommon.data_importers.tests'
-                      '.test_dataimporter_functions.empty_operation'),
-                     ('--operation=terracommon.terra.transformations'
-                      '.set_geometry_from_options'),
-                     '--layer=companies',
-                     '--key=SIREN',
-                     '--key=NIC',
-                     f'--source={test_file}')
+        with self.assertLogs(level=logging.WARNING) as cm:
+            call_command('import_csv_features',
+                         ('--operation=terracommon.data_importers.tests'
+                          '.test_dataimporter_functions.empty_operation'),
+                         ('--operation=terracommon.terra.transformations'
+                          '.set_geometry_from_options'),
+                         '--layer=companies',
+                         '--key=SIREN',
+                         '--key=NIC',
+                         f'--source={test_file}')
+        self.assertEqual(len(cm.records), 1)
+        self.assertIn('can not define geometry for:', cm.records[0].msg)
