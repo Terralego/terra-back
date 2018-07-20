ecd7577f54e759b60d1ef24658a50027c7bc7b4a
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-07-20 17:04:28 +0200

    #103 - Adding test and fixing some kind of usages

--- terracommon/core/filters.py
+++ terracommon/core/filters.py
@@ -5,28 +5,35 @@ from rest_framework.filters import OrderingFilter
 class JSONFieldOrderingFilter(OrderingFilter):
 
     def get_ordering(self, request, queryset, view):
-        ordering = super().get_ordering(request, queryset, view)
-
+        # ordering = super().get_ordering(request, queryset, view)
+        ordering = None
         params = request.query_params.get(self.ordering_param)
         if params:
             fields = [param.strip() for param in params.split(',')]
             for field in fields:
                 json_nested = field.split('__')
                 model_field = json_nested.pop(0)
-
-                if not self.remove_invalid_fields(queryset,
-                                                  [model_field, ],
-                                                  view,
-                                                  request):
-                    # The model_field must be an authorized field
-                    continue
-
-                tpl = model_field + ''.join(
-                    ['->%s' for x in range(len(json_nested))])
+                descending = False
 
                 if not ordering:
                     ordering = []
 
-                ordering.append(OrderBy(RawSQL(tpl, json_nested)))
+                if (model_field in ordering or
+                    not self.remove_invalid_fields(queryset,
+                                                   [model_field, ],
+                                                   view,
+                                                   request)):
+                    # The model_field must be an authorized field
+                    continue
+
+                if model_field.startswith('-'):
+                    descending = True
+                    model_field = model_field[1:]
+
+                tpl = model_field + ''.join(
+                    ['->%s' for x in range(len(json_nested))])
+
+                ordering.append(
+                    OrderBy(RawSQL(tpl, json_nested), descending=descending))
 
         return ordering
