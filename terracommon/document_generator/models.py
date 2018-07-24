5321cbf490c8dced78b835df468556ae86551c10
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-07-24 16:33:15 +0200

    use good string formating for date in upload_to

--- terracommon/document_generator/models.py
+++ terracommon/document_generator/models.py
@@ -1,12 +1,6 @@
-from datetime import datetime
-
 from django.db import models
-
-date = datetime.now()
 
 
 class DocumentTemplate(models.Model):
     name = models.CharField(max_length=50)
-    template = models.FileField(upload_to=('templates/'
-                                           f'{date.month}/'
-                                           f'{date.year}/'))
+    template = models.FileField(upload_to='templates/%Y/%m')
