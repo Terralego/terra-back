53cadff880f43b7b913ed58457349acb2b5d5c1d
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-07-24 16:51:42 +0200

    cache the result of pdf_generator.get_pdf in DocumentTemplateViewSets

--- terracommon/document_generator/views.py
+++ terracommon/document_generator/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
@@ -15,14 +16,20 @@ class DocumentTemplateViewSets(viewsets.ViewSet):
                   url_path='pdf/(?P<request_pk>[^/.]+)')
     def pdf_creator(self, request, pk=None, request_pk=None):
         userrequest = get_object_or_404(UserRequest, pk=request_pk)
-        myodt = get_object_or_404(DocumentTemplate, pk=pk)
+        mytemplate = get_object_or_404(DocumentTemplate, pk=pk)
 
-        myodt_path = str(myodt.template)
+        mytemplate_path = str(mytemplate.template)
+        mytemplate_name = str(mytemplate.name)
 
-        pdf_generator = DocumentGenerator(myodt_path)
+        pdf_generator = DocumentGenerator(mytemplate_path)
         pdf = pdf_generator.get_pdf(userrequest.properties)
 
-        response = HttpResponse(pdf)
+        cached_pdf = cache.get(mytemplate_name)
+        if not cached_pdf:
+            cache.set(mytemplate_name, pdf, 30)
+            cached_pdf = cache.get(mytemplate_name)
+
+        response = HttpResponse(cached_pdf)
         response['Content-Type'] = 'application/pdf'
         response['Content-disposition'] = f'attachment; filename=macaron.pdf'
         return response
