b511134d351b8632ec6c3a35ffaf81b6f0faf0ba
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-08-16 11:20:53 +0200

    add path validation in CachedDocument class + update test

--- terracommon/document_generator/tests/test_cacheddocument.py
+++ terracommon/document_generator/tests/test_cacheddocument.py
@@ -1,18 +1,23 @@
 from unittest.mock import patch
 
+from django.conf import settings
 from django.test import TestCase
 
 from terracommon.document_generator.helpers import CachedDocument
 
 
 class CachedDocumentTestCase(TestCase):
+    def setUp(self):
+        self.fake_dirname = f'{settings.MEDIA_ROOT}fake/dir/to/test'
+
     @patch('terracommon.document_generator.helpers.open')
     @patch('terracommon.document_generator.helpers.os.path')
     @patch('terracommon.document_generator.helpers.os')
     def test_create_cachefile(self, mock_os, mock_path, mock_open):
         # Force to pass in dir creation if dir don't exists
         mock_path.isdir.return_value = False
-        mock_path.dirname.return_value = 'fake/dir/to/test'
+        mock_path.dirname.return_value = self.fake_dirname
+        mock_path.abspath.return_value = self.fake_dirname
 
         # Accessing context manager open
         mock_open_ctx_manager = mock_open.return_value.__enter__.return_value
@@ -21,8 +26,8 @@ class CachedDocumentTestCase(TestCase):
         testcache.create_cache('new content', 'w')
 
         mock_path.dirname.assert_called_with(testcache.path)
-        mock_path.isdir.assert_called_with('fake/dir/to/test')
-        mock_os.makedirs.assert_called_with('fake/dir/to/test')
+        mock_path.isdir.assert_called_with(self.fake_dirname)
+        mock_os.makedirs.assert_called_with(self.fake_dirname)
         mock_open.assert_called_with(testcache.path, 'w')
         mock_open_ctx_manager.write.assert_called_with('new content')
 
@@ -35,7 +40,8 @@ class CachedDocumentTestCase(TestCase):
                                               mock_open):
         # Force "dir exists" to check if makedirs is not called
         mock_path.isdir.return_value = True
-        mock_path.dirname.return_value = 'fake/dir/for/test'
+        mock_path.dirname.return_value = self.fake_dirname
+        mock_path.abspath.return_value = self.fake_dirname
 
         # Accessing context manager open
         mock_open_ctx_manager = mock_open.return_value.__enter__.return_value
@@ -44,15 +50,21 @@ class CachedDocumentTestCase(TestCase):
         testcache.create_cache('new content', 'w')
 
         mock_path.dirname.assert_called_with(testcache.path)
-        mock_path.isdir.assert_called_with('fake/dir/for/test')
+        mock_path.isdir.assert_called_with(self.fake_dirname)
         mock_os.makedirs.assert_not_called()
         mock_open.assert_called_with(testcache.path, 'w')
         mock_open_ctx_manager.write.assert_called_with('new content')
 
     @patch('terracommon.document_generator.helpers.os')
     def test_delete_cachefile(self, mock_os):
+        mock_os.path.abspath.return_value = self.fake_dirname
+
         fake_cachename = 'fake_cache.txt'
         fake_cache = CachedDocument(fake_cachename)
         fake_cache.delete_cache()
 
         mock_os.remove.assert_called_with(fake_cache.path)
+
+    def test_unvalid_cachefile_path(self):
+        with self.assertRaises(ValueError):
+            CachedDocument('../../../etc/password')
