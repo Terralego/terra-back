c4148c3c2291760986d3e5df55f89b0532f5f184
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-08-16 17:24:46 +0200

    #155 - add tests to token url generation

--- terracommon/accounts/permissions.py
+++ terracommon/accounts/permissions.py
@@ -7,13 +7,11 @@ class TokenBasedPermission(BasePermission):
     get_user = PasswordResetConfirmView.get_user
 
     def has_permission(self, request, view):
-        assert 'uidb64' in request.GET and 'token' in request.GET
+        if 'uidb64' in request.GET and 'token' in request.GET:
+            user = self.get_user(request.GET['uidb64'])
 
-        user = self.get_user(request.GET['uidb64'])
-        print(user)
-        print(request.GET['token'])
-        if default_token_generator.check_token(user, request.GET['token']):
-            view.request.user = user
-            return True
+            if default_token_generator.check_token(user, request.GET['token']):
+                view.request.user = user
+                return True
 
         return False
