e8a4ada741e46cba0771b53eecebafe6a46e0d08
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-08-21 18:04:35 +0200

    rewrite CachedDocument and update DocumentGenerator

--- terracommon/document_generator/tests/test_documentgenerator.py
+++ terracommon/document_generator/tests/test_documentgenerator.py
@@ -1,12 +1,15 @@
 import os
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, PropertyMock, patch
 
 import requests
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from requests import HTTPError, Response
 
-from terracommon.document_generator.helpers import DocumentGenerator
+from terracommon.document_generator.helpers import (CachedDocument,
+                                                    DocumentGenerator)
+from terracommon.trrequests.tests.factories import UserRequestFactory
 
 
 class DocumentGeneratorTestCase(TestCase):
@@ -55,3 +58,30 @@ class DocumentGeneratorTestCase(TestCase):
         dg = DocumentGenerator(template)
         with self.assertRaises(HTTPError):
             dg.get_pdf({})
+
+    def test_cache_is_created(self):
+        fake_pdf = SimpleUploadedFile('fake.pdf', b'file content')
+        fake_pdf.seek(0)
+
+        with patch('requests.Response.content',
+                   new_callable=PropertyMock) as mock_content:
+            mock_content.return_value = fake_pdf
+            response_convertit = Response()
+            response_convertit.url = settings.CONVERTIT_URL
+            response_convertit.status_code = 200
+            requests.post = MagicMock(return_value=response_convertit)
+
+            odt_path = os.path.join(os.path.dirname(__file__), 'empty.odt')
+            odt_file = open(odt_path, 'rb')
+
+            userrequest = UserRequestFactory(properties={'a': 'a'})
+
+            dg = DocumentGenerator(odt_file)
+            dg.get_odt = MagicMock(return_value=odt_file)
+            fake_pdf = dg.get_pdf(userrequest)
+
+            self.assertTrue(isinstance(fake_pdf, CachedDocument))
+            self.assertTrue(os.path.isfile(fake_pdf.name))
+
+            fake_pdf.remove()
+            self.assertFalse(os.path.isfile(fake_pdf.name))
