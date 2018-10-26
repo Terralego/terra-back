fea3b2ea31233798c42210bb843720116959b140
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-10-26 16:16:04 +0200

    Add a filter the return the translated value from datastore - #238

--- terracommon/document_generator/filters.py
+++ terracommon/document_generator/filters.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.utils.dateparse import parse_datetime
 
+from terracommon.datastore.models import DataStore
+
 
 def timedelta_filter(date_value, delta_days=0):
     """ custom filter that will add a positive or negative value, timedelta
@@ -9,3 +11,11 @@ def timedelta_filter(date_value, delta_days=0):
 
     current_date = parse_datetime(date_value)
     return current_date - timedelta(days=delta_days)
+
+
+def translate_filter(data, datastorekey):
+    """ Custom filter that will return the translated data
+        from datastore correspondence table """
+
+    correspondences = DataStore.objects.get(key=datastorekey)
+    return correspondences[data]
