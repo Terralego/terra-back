2daf0f3eb41da76767bd8f38f634c749885d122d
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-10-30 17:10:32 +0100

    Update test_helpers

--- terracommon/core/tests/test_helpers.py
+++ terracommon/core/tests/test_helpers.py
@@ -53,6 +53,39 @@ class CSVTestCase(TestCase):
         csv_builder = CustomCsvBuilder(data)
         csv_builder2 = CustomCsvBuilder(data2)
         output = StringIO()
-
+        output2 = StringIO()
         csv_builder.create_csv(output)
-        csv_builder2.create_csv(output)
+        list_lines = self.get_columns(output.read())
+        self.assertEqual(sorted(list(zip(list_lines[0],
+                                         list_lines[1],
+                                         list_lines[2]
+                                         ))),
+                         [('test', 'hello-world', ''),
+                          ('value', '', ''),
+                          ('value_data_test0_fourth_events', '', '1'),
+                          ('value_data_test0_fourth_participants', '', '1'),
+                          ('value_data_test1_first_events', '1', '1'),
+                          ('value_data_test1_first_participants', '1', '1'),
+                          ('value_data_test1_second_events', '2', '8'),
+                          ('value_data_test1_second_participants', '2', '73'),
+                          ('value_data_test1_third_events', '1', '1'),
+                          ('value_data_test1_third_participants', '101',
+                           '101'),
+                          ('value_name', 'Name1', 'Name2')])
+
+        csv_builder2.create_csv(output2)
+        list_lines2 = self.get_columns(output2.read())
+        self.assertEqual(sorted(list(zip(list_lines2[0],
+                                         list_lines2[1],
+                                         ))),
+                         [('test', 'hello-world'),
+                          ('test2', 'HELLO-WORLD')])
+
+    def get_columns(self, csv):
+        list_csv = []
+        tab = csv.split('\r\n')
+        tab.pop(-1)
+        for line in tab:
+            values = line.split(',')
+            list_csv.append(values)
+        return list_csv
