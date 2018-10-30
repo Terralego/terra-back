57a701839ae6f73ac98304530999ffa1da1fc99d
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-10-30 10:16:33 +0100

    #244 - add a function to get the user from the pk so we can get any user from any pk

--- terracommon/events/signals/funcs.py
+++ terracommon/events/signals/funcs.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+
+UserModel = get_user_model()
 
 
 def users_of_group(group_name):
@@ -9,3 +12,14 @@ def users_of_group(group_name):
         }
         for user in Group.objects.get(name=group_name)
     ]
+
+
+def get_user_from_pk(pk):
+    try:
+        user = UserModel.objects.get(pk=pk)
+        return {
+            'email': user.email,
+            'properties': user.properties,
+        }
+    except UserModel.DoesNotExist:
+        return
