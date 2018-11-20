0651fbbe080091821cadc59f10cdaaeda2a5669c
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-11-20 16:40:56 +0100

    Fix tests csv_builder

--- terracommon/core/tests/test_helpers.py
+++ terracommon/core/tests/test_helpers.py
@@ -6,7 +6,7 @@ from terracommon.core.helpers import CustomCsvBuilder
 
 
 class CSVTestCase(TestCase):
-    def test_create_csv(self):
+    def test_create_csv_1(self):
         data = {"test": "hello-world",
                 "value": [
                     {"data": {
@@ -48,38 +48,35 @@ class CSVTestCase(TestCase):
                         "name": "Name2"}
                 ]
                 }
-        data2 = {"test": "hello-world", "test2": "HELLO-WORLD"}
 
         csv_builder = CustomCsvBuilder(data)
-        csv_builder2 = CustomCsvBuilder(data2)
+
         output = StringIO()
-        output2 = StringIO()
         csv_builder.create_csv(output)
         list_lines = self.get_columns(output.read())
-        self.assertEqual(sorted(list(zip(list_lines[0],
-                                         list_lines[1],
-                                         list_lines[2]
-                                         ))),
-                         [('test', 'hello-world', ''),
-                          ('value', '', ''),
-                          ('value_data_test0_fourth_events', '', '1'),
-                          ('value_data_test0_fourth_participants', '', '1'),
-                          ('value_data_test1_first_events', '1', '1'),
-                          ('value_data_test1_first_participants', '1', '1'),
-                          ('value_data_test1_second_events', '2', '8'),
-                          ('value_data_test1_second_participants', '2', '73'),
-                          ('value_data_test1_third_events', '1', '1'),
-                          ('value_data_test1_third_participants', '101',
-                           '101'),
-                          ('value_name', 'Name1', 'Name2')])
+        self.assertCountEqual(
+            list_lines,
+            [['test', 'hello-world', ''],
+             ['value', '', ''],
+             ['value_data_test0_fourth_events', '', '1'],
+             ['value_data_test0_fourth_participants', '', '1'],
+             ['value_data_test1_first_events', '1', '1'],
+             ['value_data_test1_first_participants', '1', '1'],
+             ['value_data_test1_second_events', '2', '8'],
+             ['value_data_test1_second_participants', '2', '73'],
+             ['value_data_test1_third_events', '1', '1'],
+             ['value_data_test1_third_participants', '101', '101'],
+             ['value_name', 'Name1', 'Name2']])
 
-        csv_builder2.create_csv(output2)
-        list_lines2 = self.get_columns(output2.read())
-        self.assertEqual(sorted(list(zip(list_lines2[0],
-                                         list_lines2[1],
-                                         ))),
-                         [('test', 'hello-world'),
-                          ('test2', 'HELLO-WORLD')])
+    def test_create_csv_2(self):
+        data = {"test": "hello-world", "test2": "HELLO-WORLD"}
+        csv_builder = CustomCsvBuilder(data)
+        output = StringIO()
+        csv_builder.create_csv(output)
+        list_lines = self.get_columns(output.read())
+        self.assertCountEqual(
+            list_lines,
+            [['test', 'hello-world'], ['test2', 'HELLO-WORLD']])
 
     def get_columns(self, csv):
         list_csv = []
@@ -88,4 +85,10 @@ class CSVTestCase(TestCase):
         for line in tab:
             values = line.split(',')
             list_csv.append(values)
-        return list_csv
+        by_column = []
+        for id in range(len(list_csv[0])):
+            column = []
+            for line in list_csv:
+                column.append(line[id])
+            by_column.append(column)
+        return by_column
