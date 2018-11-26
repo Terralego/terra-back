8190d5694f46a0402bd9f75c1baf77564cb95d82
Cédric Farcy <cedric-farcy@makina-corpus.com>
2018-11-26 11:18:31 +0100

    Set namespace for datastore application - #267

--- terracommon/datastore/urls.py
+++ terracommon/datastore/urls.py
@@ -2,6 +2,8 @@ from rest_framework import routers
 
 from .views import DataStoreViewSet
 
+app_name = 'datastore'
+
 router = routers.SimpleRouter()
 
 router.register(r'datastore', DataStoreViewSet, base_name='datastore')
