3dd4c415fcbb21db300e9028ed969cc6f4a6d2c5
Sebastien Corbin <seb.corbin@gmail.com>
2019-01-02 15:10:14 +0100

    Fix #291 warning on JSONField default value

--- terracommon/events/models.py
+++ terracommon/events/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class EventHandler(models.Model):
     action = models.CharField(max_length=255, blank=False, null=False)
     handler = models.CharField(max_length=255, blank=False, null=False)
-    settings = JSONField(default={})
+    settings = JSONField(default=dict)
     priority = models.PositiveIntegerField(default=10)
 
     class Meta:
