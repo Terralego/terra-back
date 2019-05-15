f57225be2451106977d6185a71f70820338bce51
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-05-15 14:18:15 +0200

    Add layer id in userrequest serializer that could be needed

--- terracommon/trrequests/serializers.py
+++ terracommon/trrequests/serializers.py
@@ -132,7 +132,7 @@ class UserRequestSerializer(serializers.ModelSerializer, SerializerCurrentUserMi
 
     class Meta:
         model = UserRequest
-        exclude = ('layer',)
+        fields = '__all__'
         read_only_fields = ('owner', 'expiry', )
 
 
@@ -182,7 +182,7 @@ class CommentSerializer(serializers.ModelSerializer,
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('owner', 'userrequest')
+        read_only_fields = ('owner', 'userrequest', 'layer')
         extra_kwargs = {
             'attachment': {'write_only': True}
         }
