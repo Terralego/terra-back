2a2814c5965fe1df9ad40625e4ae943729d217b6
J-E Castagnede <j.e.castagnede@gmail.com>
2019-09-09 10:37:12 +0200

    fix imports and isort

--- terracommon/tropp/tests/test_viewpoints.py
+++ terracommon/tropp/tests/test_viewpoints.py
@@ -8,9 +8,9 @@ from django.urls import reverse
 from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
-
 from terra.models import Feature
 from terra.tests.factories import FeatureFactory
+
 from terracommon.accounts.tests.factories import TerraUserFactory
 from terracommon.core.settings import STATES
 from terracommon.tropp.models import Picture, Viewpoint
