ecf7597ef052aab922e5739ce4e6b062f7def25f
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-09-24 17:42:12 +0200

    Ever provide serialize the id of objects, else frontend cannot edit them

--- terracommon/accounts/serializers.py
+++ terracommon/accounts/serializers.py
@@ -82,7 +82,7 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         ref_name = 'TerraGroupSerializer'
-        fields = ('name', 'users')
+        fields = ('id', 'name', 'users')
 
 
 class TerraUserSerializer(serializers.ModelSerializer):
