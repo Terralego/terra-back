82f785dbd6c893636ded6d675e22ea05b2883444
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-16 16:59:13 +0100

    fix drf 3.11 compat

--- terracommon/trrequests/urls.py
+++ terracommon/trrequests/urls.py
@@ -6,8 +6,8 @@ app_name = 'trrequests'
 
 router = routers.SimpleRouter()
 
-router.register(r'userrequest', RequestViewSet, base_name='request')
+router.register(r'userrequest', RequestViewSet, basename='request')
 router.register(r'userrequest/(?P<request_pk>\d+)/comment',
-                CommentViewSet, base_name='comment')
+                CommentViewSet, basename='comment')
 
 urlpatterns = router.urls
